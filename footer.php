317aa16e4a0d4981a70cf457ad8ead172f91b987
messikiller <messikiller@aliyun.com>
2016-09-12 17:21:11 +0800

    update utm_source

--- footer.php
+++ footer.php
@@ -33,10 +33,10 @@
                 <div class="footerwidget">
                     <h4 class="footerwidget-title"><i class="fa fa-thumbs-up"></i> 博主推荐</h4>
                     <div class="footerwidget-content footer-tag-cloud">
-                        <a href="https://phphub.org/" target="_blank" class="label mylabel-5">PHPHub论坛</a>
+                        <a href="https://phphub.org?utm_source=messikiller.net" target="_blank" class="label mylabel-5">PHPHub论坛</a>
                         <a href="http://laravelacademy.org/" target="_blank" class="label mylabel-3">laravel学院</a>
                         <a href="https://cs.phphub.org/" target="_blank" class="label mylabel-4">laravel速查表</a>
-                        <a href="http://blog.too2.net:8080" target="_blank" class="label mylabel-1">辛碌力成</a>
+                        <a href="http://blog.too2.net?utm_source=messikiller.net" target="_blank" class="label mylabel-1">辛碌力成</a>
                         <a href="http://www.bootcss.com/" target="_blank" class="label mylabel-2">bootstrap中文网</a>
                         <a href="http://www.jquery123.com/api/" target="_blank" class="label mylabel-9">jQuery API</a>
                         <a href="http://www.ghostchina.com/" target="_blank" class="label mylabel-7">Ghost中文网</a>
